608df882485e59cb4dee7555418a6e8415d4ec8a
Bonnie <blmattson@gmail.com>
2013-09-11 11:20:18 -0500

    add a failing test for topping vegetarian

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -12,8 +12,12 @@ end
 describe Pizza::Topping do
   describe '.initialize' do
     it 'sets the name of the topping' do
-      topping = Pizza::topping.new(olives)
+      topping = Pizza::Topping.new('olives')
       expect(topping.name).to eq('olives')
+    end
+    it 'sets whether or not the topping is vegetarian' do
+      topping = Pizza::Topping.new 'bell peppers', vegetarian: true
+      expect(topping.vegetarian).to eq(true)
     end
   end
 
